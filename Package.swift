a4e71fbc587ecaaaeb3e0bed906aa05143935f1b
Glenn Posadas <hello@glennvon.com>
2021-06-25 01:13:04 +0800

    fix assets for SPM.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "NumberPicker",
-      dependencies: []),
+      dependencies: [], resources: [
+        .process("Assets")
+      ]),
     .testTarget(
       name: "NumberPickerTests",
       dependencies: ["NumberPicker"]),
